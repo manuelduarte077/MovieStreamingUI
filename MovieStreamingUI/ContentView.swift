f93c2b53120e8b07d04f8f8688d6ed4ade6f92ec
Manuel Duarte <carlosd99m@gmail.com>
2022-04-06 17:47:36 -0600

    Initial Commit

---
+++ MovieStreamingUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieStreamingUI
+//
+//  Created by Manuel Duarte on 6/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!adf;d;fasd;jfkas")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieStreamingUI/MovieStreamingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieStreamingUIApp.swift
+//  MovieStreamingUI
+//
+//  Created by Manuel Duarte on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieStreamingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
